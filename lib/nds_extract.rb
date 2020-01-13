075ea3a90dbcdbe863c9c9c3cf68152ecac654f0
Jack Parker <jprkr96@gmail.com>
2020-01-13 21:52:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,10 +9,7 @@ def gross_for_director(director_data)
   while row_index < director_data.length do 
       column_index = 0 
       director_total = 0 #reintitalize value 
-      while column_index < director_data[row_index][:movies].length do 
-          director_total += director_data[row_index][:movies][column_index][:worldwide_grosses]
-              column_index += 1 
-    end 
+          director_total += director_data[row_index][:movies][:worldwide_grosses]
     return director_total
     row_index += 1 
 end
